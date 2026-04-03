1b89e99c1af60ba7f4363a76c42b46ccb4395dee
Hà Minh Phương <131509543+dsdsluc@users.noreply.github.com>
2026-04-03 08:31:43 +0700

    first commit

---
+++ nguyenminhlap_2224802010839_lab1_1/android/app/src/main/kotlin/com/example/nguyenminhlap_2224802010839_lab1_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nguyenminhlap_2224802010839_lab1_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ nguyenminhlap_2224802010839_lab1_2/test/nguyenminhlap_2224802010839_lab1_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:nguyenminhlap_2224802010839_lab1_2/nguyenminhlap_2224802010839_lab1_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
